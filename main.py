339f168e2df5fe1a8ca5b34295d210600d07222d
Superdiamant7 <superdiamant7@protonmail.com>
2024-04-14 15:43:08 +0200

    Update main.py
    Add comments and a only number generation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import customtkinter
 import random
 import pyperclip
 
-def RNombre (difficulte):
+def RNombre (difficulte):  # Génère une suite de nombres aléatoire
     i = 0
     nombre = []
     while i <= difficulte:
@@ -11,7 +11,7 @@ def RNombre (difficulte):
     tableau_str = [str(element) for element in nombre]
     return tableau_str
 
-def Rmdp (difficulte):
+def Rmdp (difficulte):  # Remplace les nombres aléatoires par des charactères aléatoires
     characteres = ['a', '3', '/', ':', 'b', '-', '1', 'c', 'd', '4', 'e', 'f', '2', 'g', '!', 'h', 'i', 'j', '#', '9', 'k', 'l', 'm', 'n', '6', 'o', '+', 'p', 'q', 'r', '5', 's', '?', '@', 't', 'u', 'v', 'w', '7', 'x', 'y', '8', 'z']
     mdp_int = RNombre(difficulte)
     mdp_str = []
@@ -21,22 +21,31 @@ def Rmdp (difficulte):
     mdp = ''.join(mdp_str)
     return mdp
 
-def copier (mdp):
+def copier (mdp):  # Copie le mot de passe généré
     pyperclip.copy(mdp)
     label_copy.configure(text="Copié ✅")
 
-def on_click ():
+def on_click ():  # Génère le mot de passe en cliquant sur le bouton
     if difficulte.get():
         if int(difficulte.get()) > 30:
-            mdp = Rmdp(30)
+            if checked() == 'on':
+                mdp = ''.join(RNombre(30))
+            else:
+                mdp = Rmdp(30)
             label_mdp2.configure(text=mdp)
         else:
-            mdp = Rmdp(int(difficulte.get()) - 1)
+            if checked() == 'on':
+                mdp = ''.join(RNombre(int(difficulte.get()) - 1))
+            else:
+                mdp = Rmdp(int(difficulte.get()) - 1)
             label_mdp2.configure(text=str(mdp))
         button_copy = customtkinter.CTkButton(window, text="Copier", width=40, command=lambda: copier(mdp))
         button_copy.place(x=175, y=300)
 
+def checked ():
+    return check_box.get()
 
+# Paramètres de la fenêtre
 window = customtkinter.CTk()
 window.title("Mot de passe aléatoire")
 window.geometry("400x350")
@@ -45,9 +54,14 @@ window.resizable(False, False)
 label = customtkinter.CTkLabel(window, text="Entrez la difficulté: ", font=("", 20))
 label.place(x=10, y=50)
 
+# Permet d'entrer la difficulté du mot de passe
 difficulte = customtkinter.CTkEntry(window, width=30, height=30)
 difficulte.place(x=280, y=50)
 
+check_box = customtkinter.CTkCheckBox(window, text="Chiffres seulement", command=checked, onvalue="on", offvalue="off")
+check_box.place(x=50, y= 100)
+
+# Bouton générer
 button = customtkinter.CTkButton(window, text="Générer", font=("", 20), command=on_click)
 button.place(x=130, y=150)
 
@@ -60,4 +74,5 @@ label_mdp2.place(x=145, y=250)
 label_copy = customtkinter.CTkLabel(window, text='', font=("", 12))
 label_copy.place(x=250, y=300)
 
+# Affiche la fenêtre
 window.mainloop()
